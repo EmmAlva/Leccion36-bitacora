18e003845c87a38d8b4529072f855545d0629ff6
EmmAlva <clioemma_28@hotmail.com>
2017-05-28 16:42:17 -0500

    modal-button +/-

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
-var inputFile = document.getElementsByClassName("file");
-	inputFile.files[0];
+// $("#myModal").modal();   AUTO-SHOW
+// 
 
-console.log(inputFile);
+var inputTitle = document.getElementById("input-title"),
+	labTitle = document.getElementById("title-lab"),
+	inputText = document.getElementById("input-texta"),
+	labText = document.getElementById("texta-lab");
+
+inputTitle.addEventListener('click', function moveLabel(){
+	// inputTitle.classList.remove('validate');
+	// inputTitle.classList.add('valid');
+	labTitle.classList.add('active');
+	labText.classList.remove('active');
+	if(inputTitle.value.length>10){
+		inputTitle.classList.remove('valid');
+		inputTitle.classList.add('invalid');
+	}
+});
+
+inputText.addEventListener('click',function moveLabel2(){
+	labTitle.classList.remove('active');
+	labText.classList.add('active');
+})
+
